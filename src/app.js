d485aa827e7faf96d431b3fdf70d0377c49e4bf5
ShahriarArefin <arfin17310@gmail.com>
2024-05-14 16:45:22 +0600

    implemented token verification

--- src/app.js
+++ src/app.js
@@ -21,6 +21,18 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// token verification middleware
+const verifyToken = (req, res, next) => {
+   const token = req.cookies?.token;
+   if (!token) return res.status(401).send({ message: "unauthorized access" });
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) return res.status(401).send({ message: "unauthorized access" });
+      req.user = decoded;
+      next();
+   });
+};
+
 (async () => {
    // ======== TOKEN ========
    const cookieOptions = {

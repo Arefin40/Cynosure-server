a5caa78e48965dc91e034eff336b4d6369c3f33b
ShahriarArefin <arfin17310@gmail.com>
2024-05-14 07:43:16 +0600

    implemented token generation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { ObjectId } = require("mongodb");
+const { connectToDB } = require("../db/database");
 
 const app = express();
 
@@ -15,8 +19,32 @@ app.use(
    })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 (async () => {
+   // ======== TOKEN ========
+   const cookieOptions = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+   };
+
+   // generate json web token
+   app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+         expiresIn: "7d",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true, token });
+   });
+
+   // Clear token on logout
+   app.get("/logout", (req, res) => {
+      res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({
+         success: true,
+      });
+   });
+
    app.get("/", async (req, res) => {
       res.send("Server is running");
    });
